421f3e309c2b5029dcfcfdc1b3dcf977760f48f7
ruthgabrielle <ruthfgms@gmail.com>
2021-07-18 11:18:01 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.{ts,tsx}',
+    ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'jsdom',
+    transform: {
+      '.+\\.(ts|tsx)$': 'ts-jest'
+    }
+  }
