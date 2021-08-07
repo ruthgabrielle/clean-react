1d9becf8e5e7d7126f5b9d0103b493e4d7e6ea8b
ruthgabrielle <ruthfgms@gmail.com>
2021-08-07 15:49:37 -0300

    feat: ensure RemoteAuthentication calss HttpPostClient w/ correct URL

---
+++ src/data/usecases/authentication/remoteAuthentication.ts
@@ -0,0 +1,11 @@
+import { HttpPostClient } from 'data/protocols/http/http-post-client'
+export class RemoteAuthenticaton {
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient
+  ) {}
+
+  async auth (): Promise<void> {
+    await this.httpPostClient.post(this.url)
+  }
+}
