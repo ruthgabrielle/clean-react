0efc555b45518351ba709b4c4baff4dcb9b117e9
ruth.gomes <ruthfgms@gmail.com>
2021-09-17 10:45:59 -0300

    chore: add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,31 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
         alias: {
             '@': path.join(__dirname, 'src')
         }
+    },
+    module: {
+        rules: [{
+            test: /\.ts(x?)$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: 'style-loader'
+            },
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: true
+                }
+            },
+            {
+                loader: 'scss-loader'
+            }]
+        }]
     },
     devServer: {
         contentBase: './public',
@@ -20,7 +41,7 @@ module.exports = {
         historyApiFallback: true
     },
     externals: {
-        react: 'React', 
+        react: 'React',
         'react-dom': 'ReactDOM'
     },
     plugins: [
